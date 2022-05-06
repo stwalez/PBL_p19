e0baab8fb3a3a1cea80f721db18ae017cbb6f11e
Olawale <stwalez1@gmail.com>
2022-05-06 09:27:39 +0100

    included egress for alb-sg

--- Terraform/modules/Security/security.tf
+++ Terraform/modules/Security/security.tf
@@ -28,28 +28,13 @@ resource "aws_security_group" "ext-alb-sg" {
       cidr_blocks = ["0.0.0.0/0"]
     }
   }
-  # ingress {
-  #   description = "HTTPS"
-  #   from_port   = 443
-  #   to_port     = 443
-  #   protocol    = "tcp"
-  #   cidr_blocks = ["0.0.0.0/0"]
-  # }
 
-  # ingress {
-  #   description = "HTTP"
-  #   from_port   = 80
-  #   to_port     = 80
-  #   protocol    = "tcp"
-  #   cidr_blocks = ["0.0.0.0/0"]
-  # }
-
-  # egress {
-  #   from_port   = 0
-  #   to_port     = 0
-  #   protocol    = "-1"
-  #   cidr_blocks = ["0.0.0.0/0"]
-  # }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
   tags = merge(
     var.tags,
